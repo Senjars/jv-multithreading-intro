33d9e79c1953f9aef9a0f5e1aa36694e5b3da69b
Senjars <jaroslawsendecki1@gmail.com>
2026-03-13 20:15:39 +0100

    Threads

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -5,7 +5,6 @@ import core.basesyntax.thread.MyThread;
 
 public class Main {
     public static void main(String[] args) {
-
         Thread thread = new MyThread();
         thread.setDaemon(true);
 
